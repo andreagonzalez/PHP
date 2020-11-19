4e52287a2d0905603e70901d145863a140f8bad1
[Andrea González] <[andreavgonzalez7@gmail.com[>
2020-11-19 20:21:10 -0300

    Exemplo Aula-03

---
+++ ola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Primeiro Exemplo PHP</title>
+<style>
+	h2 {
+		color: #80a2ff;
+		}
+</style>		
+</head>
+<body>
+	<h1>Testando PHP</h1>
+
+
+<?php
+echo "<h2>Olá Mundo!</h2>";
+?>
+
+</body>
+</html>	
